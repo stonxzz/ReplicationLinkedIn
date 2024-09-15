87d24026c82153f27ad6daf5c4965833d4bdf236
stonxzz <cuentamlgpro@gmail.com>
2024-09-15 13:07:58 -0600

    Se creo el composable con la foto de portada y la foto de perfil y se unio al setContent

--- app/src/main/java/com/example/replicationlinkedin/MainActivity.kt
+++ app/src/main/java/com/example/replicationlinkedin/MainActivity.kt
@@ -4,13 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.ArrowBack
+import androidx.compose.material.icons.sharp.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.replicationlinkedin.ui.theme.ReplicationLinkedInTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +40,67 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ReplicationLinkedInTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            Column {
+                searchBar()
+                profilePhotos()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun searchBar(){
+
+        Row (modifier = Modifier.statusBarsPadding()){
+            Icon(
+                Icons.Sharp.ArrowBack,
+                contentDescription = "Arrow back Icon",
+                modifier = Modifier
+                    .padding(7.dp)
+
+            )
+            Card(modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp))
+            {
+                Icon (
+                    Icons.Sharp.Search,
+                    contentDescription = "Search Icon",
+                    modifier = Modifier
+                        .padding(4.dp)
+                )
+            }
+        }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ReplicationLinkedInTheme {
-        Greeting("Android")
+fun profilePhotos(){
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(1.dp)
+    ){
+        Image(painterResource(R.drawable.galaxia),
+            contentDescription = "Imagen de portada",
+            contentScale =  ContentScale.FillWidth,
+            modifier = Modifier.height(125.dp)
+                .padding(top = 2.dp))
+
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp, 50.dp),
+            horizontalArrangement = Arrangement.Start) {
+            Image(painterResource(R.drawable.perfil),
+                contentDescription = "Imagen de perfil",
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier
+                    .size(140.dp)
+                    .clip(CircleShape)
+                    )
+        }
     }
 }
+
+
